4bf564b6918cf8fe8bb3390f4ef007fed74b3378
Josip Grgic <josip.grgic@syskit.com>
2021-12-03 02:10:04 +0100

    days 1 and 2

---
+++ 2021/day-01/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("./input.txt");
+            // var lines = File.ReadAllLines("./input.example");
+
+            var depths = lines.Select(x => int.Parse(x.Trim())).ToArray();
+
+            Second(depths);
+
+
+        }
+
+        static void First(int[] depths)
+        {
+            var count = 0;
+
+            for (var i = 0; i < depths.Count() - 1; i++)
+            {
+                var firstNumber = depths[i];
+                var secondNumber = depths[i + 1];
+
+                count += secondNumber > firstNumber ? 1 : 0;
+            }
+
+            Console.WriteLine(count);
+        }
+
+        static void Second(int[] depths)
+        {
+            var count = 0;
+
+            for (var i = 0; i < depths.Count() - 3; i++)
+            {
+                var firstWindowSum = depths[i..(i + 3)].Sum();
+                var secondWindowSum = depths[(i + 1)..(i + 4)].Sum();
+
+                count += secondWindowSum > firstWindowSum ? 1 : 0;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ 2021/day-02/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+
+namespace Day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("./input.txt");
+            // var lines = File.ReadAllLines("./input.example");
+
+            var commands = lines.Select(x =>
+                {
+                    var match = Regex.Matches(x, @"(\w*) (\d*)", RegexOptions.IgnoreCase);
+                    return (Position: match[0].Groups[1].Value, Amount: int.Parse(match[0].Groups[2].Value));
+                }).ToList();
+
+
+            First(commands);
+        }
+
+        static void First(List<(string Position, int Amount)> commands)
+        {
+            var horizontal = 0;
+            var depth = 0;
+            var aim = 0;
+
+            commands.ForEach(x =>
+            {
+                switch (x.Position)
+                {
+                    case "forward":
+                        horizontal += x.Amount;
+                        depth += aim * x.Amount;
+                        break;
+                    case "down":
+                        aim += x.Amount;
+                        break;
+                    case "up":
+                        aim -= x.Amount;
+                        break;
+                    default:
+                        throw new Exception(x.Position);
+                }
+
+            });
+
+            Console.WriteLine(horizontal * depth);
+        }
+    }
+}
